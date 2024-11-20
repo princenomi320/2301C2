6acea7d3da8f4bb1687bb53d2bbbcacb9d6fe35c
MAHNOOR USMAN <mahnoorfgs@gmail.com>
2024-11-20 11:23:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Button clicked!');
+        });
+    });
+});
